c9df14b61fee7c7c77dbcd2e84d00ddee9472821
151044 <yhchowai@connect.ust.hk>
2024-02-29 22:13:12 +0800

    fix: Unnecessary recompilation for PRs

--- src/main/java/hk/ust/comp4321/db/DatabaseConnection.java
+++ src/main/java/hk/ust/comp4321/db/DatabaseConnection.java
@@ -1,5 +1,18 @@
 package hk.ust.comp4321.db;
 
-public class DatabaseConnection {
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DatabaseConnection implements AutoCloseable {
+    private Connection conn;
+    public DatabaseConnection(Path path) throws SQLException {
+        conn = DriverManager.getConnection("jdbc:sqlite:" + path.toAbsolutePath());
+    }
+
+    @Override
+    public void close() throws Exception {
+         conn.close();
+    }
 }
