e60c5592dc85687b79cb305a258f896b2d63f1cb
151044 <yhchowai@connect.ust.hk>
2024-03-15 14:31:52 +0800

    misc: Code cleanup

--- src/test/java/hk/ust/comp4321/db/TableOperationTest.java
+++ src/test/java/hk/ust/comp4321/db/TableOperationTest.java
@@ -12,7 +12,6 @@ import java.nio.file.Path;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TableOperationTest {
     private DatabaseConnection conn;
