a489b7a0c1966cbed00066f8b6179bee67a32db2
151044 <yhchowai@connect.ust.hk>
2024-03-20 18:39:09 +0800

    refactor: Change Stopword to use ResourceLoader

--- src/main/java/hk/ust/comp4321/util/StopWord.java
+++ src/main/java/hk/ust/comp4321/util/StopWord.java
@@ -1,28 +1,38 @@
 package hk.ust.comp4321.util;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * Class to represent the list of stopwords.
+ */
 public class StopWord {
-    private static Set<String> stopWords;
+    private static final Set<String> stopWords;
 
     static {
         try {
-            stopWords = new HashSet<>(Files.readAllLines(Path.of("data/nltk_stopwords.txt")));
+            stopWords = new BufferedReader(new InputStreamReader(
+                    ResourceLoader.loadResource(Path.of("data/nltk_stopwords.txt"))))
+                    .lines().collect(Collectors.toSet());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Checks if a word is in the stop word list.
+     * @param str The string to check
+     * @return True if the word is a stopword; false otherwise
+     */
     public static boolean isStopWord(String str) {
         return stopWords.contains(str);
     }
-
-
 
     private StopWord() {}
 }
