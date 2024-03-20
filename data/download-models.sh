0f03dfc0e2f7d69e3582020df41f6318ce72d9dd
151044 <yhchowai@connect.ust.hk>
2024-03-20 13:42:25 +0800

    feat: Scripts for downloading models from OpenNLP

---
+++ data/download-models.sh
@@ -0,0 +1,2 @@
+wget https://dlcdn.apache.org/opennlp/models/ud-models-1.0/opennlp-en-ud-ewt-sentence-1.0-1.9.3.bin -O sentence-model-en.bin
+wget https://dlcdn.apache.org/opennlp/models/ud-models-1.0/opennlp-en-ud-ewt-tokens-1.0-1.9.3.bin -O token-model-en.bin
