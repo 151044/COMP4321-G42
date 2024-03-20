a9c159f0da26df374d0539d5a811caf0929bf362
151044 <yhchowai@connect.ust.hk>
2024-03-20 15:49:37 +0800

    fix: Patch script to download files to the correct place

--- data/download-models.sh
+++ data/download-models.sh
@@ -1,2 +1,3 @@
-wget https://dlcdn.apache.org/opennlp/models/ud-models-1.0/opennlp-en-ud-ewt-sentence-1.0-1.9.3.bin -O sentence-model-en.bin
-wget https://dlcdn.apache.org/opennlp/models/ud-models-1.0/opennlp-en-ud-ewt-tokens-1.0-1.9.3.bin -O token-model-en.bin
+# Note: Please run this script from the project directory
+wget https://dlcdn.apache.org/opennlp/models/ud-models-1.0/opennlp-en-ud-ewt-sentence-1.0-1.9.3.bin -O data/sentence-model-en.bin
+wget https://dlcdn.apache.org/opennlp/models/ud-models-1.0/opennlp-en-ud-ewt-tokens-1.0-1.9.3.bin -O data/token-model-en.bin
