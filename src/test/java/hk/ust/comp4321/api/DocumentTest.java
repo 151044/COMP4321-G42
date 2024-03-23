d58d673ac70703fa6da4b097d03cdce7d7ab98a7
Duppy-Conqueror <duppyconqueror206@gmail.com>
2024-03-23 23:19:13 +0800

    fix: Fixed testcase DocumentTest.retrieveFromWebBody

--- src/test/java/hk/ust/comp4321/api/DocumentTest.java
+++ src/test/java/hk/ust/comp4321/api/DocumentTest.java
@@ -53,21 +53,19 @@ public class DocumentTest {
 
         Map<String, WordInfo> expectedMap = new HashMap<>();
 
-        expectedMap.put("thi", new WordInfo(5, 0, 0, 0, "s"));
         expectedMap.put("test", new WordInfo(5, 0, 0, 3, ""));
         expectedMap.put("page", new WordInfo(5, 0, 0, 4, ""));
         expectedMap.put("crawler", new WordInfo(5, 0, 0,7, ""));
-        expectedMap.put("befor", new WordInfo(5, 1, 0, 0, "e"));
-        expectedMap.put("gett", new WordInfo(5, 1, 0,1, "ing"));
+        expectedMap.put("gett", new WordInfo(5, 1, 0,1, "getting"));
         expectedMap.put("admission", new WordInfo(5, 1, 0, 3, ""));
         expectedMap.put("cse", new WordInfo(5, 1, 0,5, ""));
-        expectedMap.put("depart", new WordInfo(5, 1, 0, 6, "ment"));
+        expectedMap.put("depart", new WordInfo(5, 1, 0, 6, "department"));
         expectedMap.put("hkust", new WordInfo(5, 1, 0,8, ""));
         expectedMap.put("read", new WordInfo(5, 1, 0, 11, ""));
-        expectedMap.put("internat", new WordInfo(5, 1, 0,14, "ional"));
+        expectedMap.put("internat", new WordInfo(5, 1, 0,14, "international"));
         expectedMap.put("news", new WordInfo(5, 1, 0,15, ""));
-        expectedMap.put("book", new WordInfo(5, 1, 0,18, "s"));
-        expectedMap.put("movi", new WordInfo(5, 2, 0,3, "e"));
+        expectedMap.put("book", new WordInfo(5, 1, 0,18, "books"));
+        expectedMap.put("movi", new WordInfo(5, 2, 0,3, "movie"));
         expectedMap.put("list", new WordInfo(5, 2, 0,4, ""));
         expectedMap.put("new", new WordInfo(5, 2, 0,5, ""));
 
