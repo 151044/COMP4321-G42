fcad508839ec94175a59b023857c303430b74365
Phantasmysteria <wallacechan58185@gmail.com>
2024-03-28 11:21:15 +0800

    test: Fix test setUp to reset nextDocId

--- src/test/java/hk/ust/comp4321/spider/SpiderTest.java
+++ src/test/java/hk/ust/comp4321/spider/SpiderTest.java
@@ -2,6 +2,7 @@ package hk.ust.comp4321.spider;
 
 import hk.ust.comp4321.api.Document;
 import hk.ust.comp4321.db.DatabaseConnection;
+import hk.ust.comp4321.test.ReflectUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,7 +26,9 @@ public class SpiderTest {
     DatabaseConnection conn;
 
     @BeforeEach
-    void setUp() throws IOException, SQLException {
+    void setUp() throws IOException, SQLException, NoSuchFieldException, IllegalAccessException {
+        // Reset next doc id
+        ReflectUtil.setStaticField("nextDocId", null, DatabaseConnection.class);
         Files.deleteIfExists(testPath);
         testLink = new URL("https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm");
         testLinkExtra = new URL("https://www.google.com/comp4321");
@@ -45,10 +48,12 @@ public class SpiderTest {
         // Crawl 10 pages
         assertEquals(10, testSpider.discover(10).size());
 
-        // Modify the last modified date
-        conn.insertDocument(new Document(URI.create("https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm").toURL(),0, Instant.ofEpochMilli(4321L), 603L));
-        // Spider should update the page
-        assertEquals(List.of(new URL("https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm")), testSpider.discover(1));
+        // Modify the last modified date of a child page
+        conn.insertDocument(new Document(URI.create("https://www.cse.ust.hk/~kwtleung/COMP4321/ust_cse.htm").toURL(),1, Instant.ofEpochMilli(4321L), 603L));
+        // Spider should update the child page, and crawl 2 new pages
+        assertEquals(List.of(new URL("https://www.cse.ust.hk/~kwtleung/COMP4321/ust_cse.htm"),
+                new URL("https://www.cse.ust.hk/~kwtleung/COMP4321/books/book2.htm"),
+                new URL("https://www.cse.ust.hk/~kwtleung/COMP4321/books/book3.htm")), testSpider.discover(3));
 
         // HTTP status code check and total pages < threshold break test
         assertEquals(List.of(), testSpiderExtra.discover(2));
