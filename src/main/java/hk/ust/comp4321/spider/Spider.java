7afddc6d118b3bc672e366d6c676487372cf61af
151044 <yhchowai@connect.ust.hk>
2024-03-30 10:25:15 +0800

    fix: Change spider to only connect once

--- src/main/java/hk/ust/comp4321/spider/Spider.java
+++ src/main/java/hk/ust/comp4321/spider/Spider.java
@@ -81,14 +81,16 @@ public class Spider {
             try {
                 Connection currConnection = Jsoup.connect(currentURL.toString());
                 org.jsoup.nodes.Document jsoupDoc;
+                Connection.Response response;
 
                 try {
-                    jsoupDoc = currConnection.get();
+                    response = currConnection.execute();
+                    jsoupDoc = response.parse();
                 } catch (IOException e) {
+                    System.err.println("Unable to connect to " + currentURL);
+                    e.printStackTrace();
                     continue;
                 }
-
-                Connection.Response response = currConnection.execute();
 
                 Instant lastModifiedDate = Instant.from(DateTimeFormatter.RFC_1123_DATE_TIME.parse(response.header(response.hasHeader("Last-Modified") ? "Last-Modified" : "Date")));
 
@@ -137,7 +139,7 @@ public class Spider {
                         }
                     }
                 }
-            } catch (HttpStatusException | ValidationException e) {
+            } catch (ValidationException e) {
                 System.err.println("Warning: Unable to crawl " + currentURL + ".");
                 e.printStackTrace();
             }
