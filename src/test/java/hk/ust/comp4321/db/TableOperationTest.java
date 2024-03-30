951310eeec92f0191437bbd694bd1820b9d1de15
151044 <yhchowai@connect.ust.hk>
2024-03-30 09:10:37 +0800

    test: Drop usages of getFrequency(int) and replaced them with getFrequency(int, int_

--- src/test/java/hk/ust/comp4321/db/TableOperationTest.java
+++ src/test/java/hk/ust/comp4321/db/TableOperationTest.java
@@ -74,9 +74,9 @@ class TableOperationTest {
         body.insertWordInfo(1, new WordInfo(0, 0, 0, 0, "location"));
         title.insertWordInfo(2, new WordInfo(0, 0, 0, 0, "opportunist"));
 
-        assertTrue(body.getFrequency(1).contains(new WordInfo(0, 0, 0, 0, "location")));
-        assertTrue(title.getFrequency(2).contains(new WordInfo(0, 0, 0, 0, "opportunist")));
-        assertFalse(title.getFrequency(2).contains(new WordInfo(0, 0, 0, 0, "opportunion")));
+        assertTrue(body.getFrequency(1, 0).contains(new WordInfo(0, 0, 0, 0, "location")));
+        assertTrue(title.getFrequency(2, 0).contains(new WordInfo(0, 0, 0, 0, "opportunist")));
+        assertFalse(title.getFrequency(2, 0).contains(new WordInfo(0, 0, 0, 0, "opportunion")));
 
         assertDoesNotThrow(() -> body.insertWordInfo(1, new WordInfo(0, 0, 0, 0, "location")));
         assertDoesNotThrow(() -> title.insertWordInfo(0, new WordInfo(0, 1, 1, 1, "computing")));
@@ -84,20 +84,19 @@ class TableOperationTest {
 
     @Test
     void getFrequency() {
-        assertTrue(body.getFrequency(0).contains(new WordInfo(1, 3270972, 2, 1, "computer")));
-        assertFalse(body.getFrequency(0).contains(new WordInfo(1, 3270972, 2, 239040, "computer")));
+        assertTrue(body.getFrequency(0, 1).contains(new WordInfo(1, 3270972, 2, 1, "computer")));
+        assertFalse(body.getFrequency(0, 1).contains(new WordInfo(1, 3270972, 2, 239040, "computer")));
     }
 
     @Test
     void deleteFrequencies() {
         body.deleteFrequencies(0);
-        assertTrue(body.getFrequency(body.getIdFromStem("comput"))
-                .stream().noneMatch(w -> w.docId() == 0)); // body tables don't have docId == 0
-        assertTrue(body.getFrequency(body.getIdFromStem("comput")).stream().noneMatch(w -> w.docId() == 0)); // title table don't have docId == 0
-        assertEquals(2, body.getFrequency(body.getIdFromStem("comput")).size()); // 2 frequency records remaining
+        assertEquals(0, body.getFrequency(body.getIdFromStem("comput"), 0).size()); // body tables don't have docId == 0
+        assertEquals(0, body.getFrequency(body.getIdFromStem("comput"), 0).size()); // title table don't have docId == 0
+        assertEquals(2, body.getFrequency(body.getIdFromStem("comput"), 1).size()); // 2 frequency records remaining
 
         title.deleteFrequencies(0);
-        assertEquals(1, title.getFrequency(title.getIdFromStem("comput")).size());
+        assertEquals(1, title.getFrequency(title.getIdFromStem("comput"), 1).size());
     }
 
     @Test
