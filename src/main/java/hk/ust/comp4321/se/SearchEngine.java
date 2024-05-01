e823fdde6cc462480759d9f9ac534aa1a813c44f
151044 <yhchowai@connect.ust.hk>
2024-05-01 13:07:55 +0800

    feat: Simple tf * idf / max(tf) search engine implementation

--- src/main/java/hk/ust/comp4321/se/SearchEngine.java
+++ src/main/java/hk/ust/comp4321/se/SearchEngine.java
@@ -2,20 +2,24 @@ package hk.ust.comp4321.se;
 
 import hk.ust.comp4321.api.Document;
 import hk.ust.comp4321.db.DatabaseConnection;
+import hk.ust.comp4321.util.Tuple;
 
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 
 public class SearchEngine {
 
-    DatabaseConnection conn;
+    private DatabaseConnection conn;
+    private final List<Document> docs;
+    private final double TITLE_BOOST_FACTOR = 1.5;
 
     /***
      * Creates a new Search Engine.
      * @param conn The DatabaseConnection to use
      */
-    public SearchEngine(DatabaseConnection conn) {
+    public SearchEngine(DatabaseConnection conn, List<Document> docs) {
         this.conn = conn;
+        this.docs = docs;
     }
 
     /***
@@ -23,7 +27,7 @@ public class SearchEngine {
      * @param query The entire input query
      * @return List of Document-score pairs sorted by score in non-increasing order
      */
-    public List<Map<Document, Double>> search(String query) {
+    public List<Tuple<Document, Double>> search(SearchVector query) {
         return search(query, 50);
     }
 
@@ -33,8 +37,13 @@ public class SearchEngine {
      * @param numDocs The number of Documents to search for
      * @return List of Document-score pairs sorted by score in non-increasing order
      */
-    public List<Map<Document, Double>> search(String query, int numDocs) {
-        return List.of();
+    public List<Tuple<Document, Double>> search(SearchVector query, int numDocs) {
+        return docs.stream()
+                .map(d -> new Tuple<>(d, d.asTitleVector(conn).cosineSim(query) * TITLE_BOOST_FACTOR +
+                        d.asBodyVector(conn).cosineSim(query)))
+                .sorted(Comparator.<Tuple<Document, Double>, Double>comparing(Tuple::right).reversed())
+                .limit(numDocs)
+                .toList();
     }
 
 }
