22cebbdb656f743eeb9c9b0e8003fd77333af8ef
151044 <yhchowai@connect.ust.hk>
2024-05-07 18:12:12 +0800

    feat: Strip stopwords from quotes String

--- src/main/java/hk/ust/comp4321/util/StopWord.java
+++ src/main/java/hk/ust/comp4321/util/StopWord.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -30,6 +32,26 @@ public class StopWord {
      */
     public static boolean isStopWord(String str) {
         return stopWords.contains(str);
+    }
+
+    /**
+     * Removes all stop words from a specified string.
+     * @param words The string to strip the stop words from
+     * @return The original string with all stopwords removed
+     */
+    public static String stripStopwords(String words) {
+        List<String> tokenized = new ArrayList<>(List.of(words.split(" ")));
+        StringBuilder sb = new StringBuilder(" ");
+        for (int i = 0; i < tokenized.size(); i++) {
+            String stripped = tokenized.get(i).replace("\"", "");
+            if (stopWords.contains(stripped)) {
+                sb.append(tokenized.get(i).contains("\"") ? "\"" : "");
+            } else {
+                sb.append(" ").append(tokenized.get(i));
+            }
+        }
+        System.out.println(sb.toString().replace(" \" ", " \"").strip());
+        return sb.toString().replace(" \" ", " \"").strip();
     }
 
     private StopWord() {}
